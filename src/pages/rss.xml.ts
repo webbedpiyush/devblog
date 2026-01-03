8071826f16cfa6ec303c3afd9edd90d9b3d498ca
Piyush Tiwari <piyushtiwarindls220@gmail.com>
2026-01-03 23:19:00 +0530

    created fuzzy search for blogs & rss feed generator too

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import getSortedPosts from "@utils/getSortedPosts";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE } from "@config";
+
+export async function GET() {
+  const posts = await getCollection("blog");
+  const sortedPosts = getSortedPosts(posts);
+
+  return rss({
+    title: SITE.title,
+    description: SITE.desc,
+    site: SITE.website,
+    items: sortedPosts.map(({ data, slug }) => ({
+      link: `posts/${slug}/`,
+      title: data.title,
+      description: data.description,
+      pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+    })),
+  });
+}
